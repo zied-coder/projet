00348c5c3717e704431ac2009e5b2bbab97f78f6
zied-coder <hechmizied87@gmail.com>
2022-07-22 12:45:16 +0100

    first commit

---
+++ file.js
@@ -0,0 +1,39 @@
+// change navbar style on scroll
+window.addEventListener('scroll', () => { 
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0 )
+}
+)
+ const faqs = document.querySelectorAll('.faq');
+ faqs.forEach ( faq => { 
+    faq.addEventListener('click', () => { 
+        faq.classList.toggle('open');
+        //change icon 
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') { 
+            icon.className = "uil uil-minus";
+        } else { 
+            icon.className = "uil uil-plus";
+        }
+    })
+})
+
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open_menu_btn");
+const closeBtn = document.querySelector("#close_menu_btn"); 
+
+console.log(menuBtn)
+menuBtn.addEventListener('click', function () {
+        menu.style.display = "flex ";
+        closeBtn.style.display = "inline-block";
+        menuBtn.style.display = "none";
+
+ })
+
+const closeNav = ()  => {
+    menu.style.display = "none ";
+    closeBtn.style.display = "none ";
+    menuBtn.style.display = "inline-block";
+
+}
+
+closeBtn.addEventListener('click', closeNav )
